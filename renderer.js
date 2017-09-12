93fe4080eae90064ffed3d041dc009054f655276
android.zyy <2811251638@qq.com>
2017-09-12 17:47:56 +0800

    upd: 新建electron项目

--- renderer.js
+++ renderer.js
@@ -1,3 +1,10 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+let myNotification = new Notification('新版本！！！', {
+  body: '有新版本，正在更新中。。。'
+})
+
+myNotification.onclick = () => {
+  console.log('Notification clicked')
+}
